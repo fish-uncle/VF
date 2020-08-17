cfa31039c77b55dd388ece714fcdaee5e0beea8a
fish-uncle <fishuncle@126.com>
2020-08-17 16:03:10 +0800

    with cdn

--- fish.config.js
+++ fish.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 module.exports = {
   dev: {
-    template: path.join(__dirname, 'src/index-dev.html')
+    template: path.join(__dirname, 'src/index-dev.html'),
   },
   prod: {
-    template: path.join(__dirname, 'src/index-prod.html')
+    template: path.join(__dirname, 'src/index-prod.html'),
+    publicPath: 'https://cdn.shenzhepei.com/VF/prod/'
   },
 };
